d15fe77c6e9de4ac17ccf7b796a04277fc1409f7
Mahimahto <manisha.tkg08@gmail.com>
2023-10-03 16:07:15 +0530

    I add to on-and-off-img

--- on-and-off-switch.js
+++ on-and-off-switch.js
@@ -1,23 +1,22 @@
-let btn = document.querySelector("#button")
-let vlu = 0;
-button.addEventListener('click', function () {
-    if (vlu === 0) {
-        button.innerHTML = "Trun On"
-        button.style.backgroundColor = "green"
-        vlu = 1;
-    }
-    else {
-        button.innerHTML = "Trun Off"
-        button.style.backgroundColor = "white"
-        vlu = 0;
+const button = document.querySelector(".button")
+let com = 0;
 
+button.addEventListener("click", function () {
+
+    button.style.backgroundColor = "green"
+
+
+    if (com === 0) {
+        button.innerHTML = "turn On"
+        let img = document.querySelector("#image")
+        img.setAttribute("src", "bulb-on.png")
+        com = 1
+
+    } else {
+        button.innerHTML = "turn Off "
+        button.style.backgroundColor = "white"
+        let img = document.querySelector("#image")
+        img.setAttribute("src", "bulb-off.png")
+        com = 0
     }
 })
-
-
-
-
-
-
-
-
